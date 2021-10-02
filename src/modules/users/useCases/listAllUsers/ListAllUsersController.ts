558276b42667ca9f826052c5c8548f8e61fadc42
Matheus Santos <matheus.pires@sp.senai.br>
2021-10-02 11:50:18 -0300

    feat: finish code challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -1,12 +1,22 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { ListAllUsersUseCase } from "./ListAllUsersUseCase";
+import { ListAllUsersUseCase } from './ListAllUsersUseCase';
 
 class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.headers;
+
+      const userId = String(user_id);
+
+      const users = this.listAllUsersUseCase.execute({ user_id: userId });
+
+      return response.json(users);
+    } catch (error) {
+      return response.status(400).json({ error });
+    }
   }
 }
 
